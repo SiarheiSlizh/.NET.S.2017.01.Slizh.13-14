2f80f28d1de0e747b6c6a7f9e9679bfb8f525181
сергей слиж <sergeislizh@mail.ru>
2017-04-29 22:12:03 +0300

    add task 4

--- Task4/DiagonalMatrix.cs
+++ Task4/DiagonalMatrix.cs
@@ -6,16 +6,38 @@ using System.Threading.Tasks;
 
 namespace Task4
 {
+    /// <summary>
+    /// generic class contains main information about diagonal matrix
+    /// </summary>
+    /// <typeparam name="T">Type which closes class</typeparam>
     public class DiagonalMatrix<T>:Matrix<T>
     {
+        #region ctor
+        /// <summary>
+        /// create diagonal matrix with specified size
+        /// </summary>
+        /// <param name="size">size of matrix</param>
         public DiagonalMatrix(int size):base(size)
         {
         }
 
+        /// <summary>
+        /// create diagonal matrix which is based on existing matrix
+        /// </summary>
+        /// <param name="size">size of matrix</param>
+        /// <param name="array">existing matrix</param>
         public DiagonalMatrix(int size, T[,] array) : base(size,array)
         {
         }
+        #endregion
 
+        #region Api
+        /// <summary>
+        /// checks a possibility to create diagonal matrix based on existing matrix
+        /// </summary>
+        /// <param name="size">size of matrix</param>
+        /// <param name="array">existing matrix</param>
+        /// <returns>true in case possibility to create diagonal matrix based on existing matrix else false</returns>
         public override bool CheckExisting(int size, T[,] array)
         {
             if (ReferenceEquals(array, null))
@@ -33,6 +55,12 @@ namespace Task4
             return true;
         }
 
+        /// <summary>
+        /// Method starts after event running.
+        /// </summary>
+        /// <param name="sender">object which runs event</param>
+        /// <param name="e">additional information about diagonal matrix</param>
         protected override void Info(object sender, MatrixEventArgs e) => Console.WriteLine($"Diagonal matrix[{e.Rows},{e.Columns}] is changed !");
+        #endregion
     }
 }
--- Task4/Matrix.cs
+++ Task4/Matrix.cs
@@ -7,31 +7,68 @@ using System.Threading.Tasks;
 
 namespace Task4
 {
+    /// <summary>
+    /// Abstract generic class contains main information about matrix
+    /// </summary>
+    /// <typeparam name="T">Type which closes class</typeparam>
     public abstract class Matrix<T>
     {
+        #region private fields
+        /// <summary>
+        /// Two-dimensional array which is one of matrix's type.
+        /// </summary>
         private T[,] matrix;
+        #endregion
 
+        #region prop
+        /// <summary>
+        /// Amount of elements in matrix
+        /// </summary>
+        public int Length { get { return matrix.Length; } }
+
+        /// <summary>
+        /// Indexer which allows to work safely elements in matrix 
+        /// </summary>
+        /// <param name="i">row of matrix</param>
+        /// <param name="j">column of matrix</param>
+        /// <returns>element of matrix</returns>
         public T this[int i, int j]
         {
             get {
-                if (i > Math.Sqrt(matrix.Length) - 1 || j > Math.Sqrt(matrix.Length) - 1 || i < 0 || j < 0)
+                if (i > Math.Sqrt(Length) - 1 || j > Math.Sqrt(Length) - 1 || i < 0 || j < 0)
                     throw new ArgumentOutOfRangeException(nameof(matrix));
                 return matrix[i, j];
             }
             set {
-                if (i > Math.Sqrt(matrix.Length) - 1 || j > Math.Sqrt(matrix.Length) - 1 || i < 0 || j < 0)
+                if (i > Math.Sqrt(Length) - 1 || j > Math.Sqrt(Length) - 1 || i < 0 || j < 0)
                     throw new ArgumentOutOfRangeException(nameof(matrix));
                 matrix[i, j] = value;
             }
         }
+        #endregion
 
+        #region event
+        /// <summary>
+        /// event which starts in case changing element in matrix
+        /// </summary>
         public event EventHandler<MatrixEventArgs> changeMatrix = delegate { };
+        #endregion
 
+        #region ctor
+        /// <summary>
+        /// create matrix with specified size
+        /// </summary>
+        /// <param name="size">size of matrix</param>
         public Matrix(int size)
         {
             matrix = new T[size, size];
         }
 
+        /// <summary>
+        /// create matrix which is based on existing matrix
+        /// </summary>
+        /// <param name="size">size of matrix</param>
+        /// <param name="array">existing matrix</param>
         public Matrix(int size, T[,] array)
         {
             if (CheckExisting(size, array))
@@ -42,13 +79,39 @@ namespace Task4
             else
                 throw new ArgumentException(nameof(array));
         }
+        #endregion
 
+        #region API
+        /// <summary>
+        /// checks a possibility to create matrix based on existing matrix
+        /// </summary>
+        /// <param name="size">size of matrix</param>
+        /// <param name="array">existing matrix</param>
+        /// <returns>true in case possibility to create matrix based on existing matrix else false</returns>
         public abstract bool CheckExisting(int size, T[,] array);
 
-        public void Register(Matrix<T> matrix) => matrix.changeMatrix += Info;
+        /// <summary>
+        /// Adds new operation to classes inheritance
+        /// </summary>
+        /// <param name="visitor">object of class with additional information</param>
+        public void Accept(IMatrixVisitor<T> visitor) => visitor.Visit((dynamic)this);
 
-        public void Unregister(Matrix<T> matrix) => matrix.changeMatrix -= Info;
+        /// <summary>
+        /// Registrate method on event
+        /// </summary>
+        public void Register() => this.changeMatrix += Info;
 
+        /// <summary>
+        /// Unregistrate method fro event
+        /// </summary>
+        public void Unregister() => this.changeMatrix -= Info;
+
+        /// <summary>
+        /// Change element in matrix
+        /// </summary>
+        /// <param name="row">row of matrix</param>
+        /// <param name="column">column of matrix</param>
+        /// <param name="element">element which is replaced with element[row,column]</param>
         public void ChangeElement(int row, int column, T element)
         {
             MatrixEventArgs changeElement = new MatrixEventArgs(row, column);
@@ -56,20 +119,51 @@ namespace Task4
             OnChangeMatrix(changeElement);
         }
 
-        protected virtual void Info(object sender, MatrixEventArgs e) => Console.WriteLine($"matrix[{e.Rows},{e.Columns}] is changed !");
+        /// <summary>
+        /// Method starts after event running.
+        /// </summary>
+        /// <param name="sender">object which runs event</param>
+        /// <param name="e">additional information about object</param>
+        protected abstract void Info(object sender, MatrixEventArgs e);
+        #endregion
 
+        #region private methods
+        /// <summary>
+        /// method starts event
+        /// </summary>
+        /// <param name="e">additional information about object</param>
         private void OnChangeMatrix(MatrixEventArgs e) => changeMatrix?.Invoke(this, e);
+        #endregion
     }
 
+    /// <summary>
+    /// class contains additional data about changable object
+    /// </summary>
     public class MatrixEventArgs:EventArgs
     {
+        #region prop
+        /// <summary>
+        /// row of matrix
+        /// </summary>
         public int Rows { get; }
-        public int Columns { get; }
 
+        /// <summary>
+        /// column of matrix
+        /// </summary>
+        public int Columns { get; }
+        #endregion
+
+        #region ctor
+        /// <summary>
+        /// create object with information about changable element
+        /// </summary>
+        /// <param name="rows">row of matrix</param>
+        /// <param name="columns">column of matrix</param>
         public MatrixEventArgs(int rows, int columns)
         {
             Rows = rows;
             Columns = columns;
         }
+        #endregion
     }
 }
